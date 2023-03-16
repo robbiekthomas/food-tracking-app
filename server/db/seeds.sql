ec7b155411aadfc690365a72b0f78941ea0735fc
Lauren Johnston <hilotraining@outlook.com>
2023-03-16 10:37:06 -0600

    Finished skeleton for seeds file

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -6,17 +6,23 @@ INSERT INTO userDetails (weight, height, weight_units, measurement_units, body_f
 VALUES (150, 170, "lb", "cm", 25, false, true, "lose fat");
 
 
-INSERT INTO user_edits ()
+INSERT INTO user_edits (user_id, user_Details)
+VALUES (0,0);
+
+
+INSERT INTO habitGoals (name)
+VALUES ("Eat more Vegetables");
+
+
+INSERT INTO habitGoal_logs (goal_id, user_id)
+VALUES (0, 0);
+
+
+INSERT INTO foods (name, grams_per_serving, carbs, fat, protein)
 VALUES ();
 
+INSERT INTO meals (name)
+VALUES ("Breakfast");
 
-INSERT INTO habitGoals ()
-VALUES ();
-
-
-INSERT INTO habitGoal_logs ()
-VALUES ();
-
-
-INSERT INTO food_logs ()
-VALUES ();
+INSERT INTO food_logs (food_id, user_id, meal_id, servings)
+VALUES (0, 0, 0, 0);
