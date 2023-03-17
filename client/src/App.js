97378f5f7ede8c124675de53a6d93ad53a7507c6
Lauren Johnston <hilotraining@outlook.com>
2023-03-17 13:47:28 -0600

    retreived axios call

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [test, setTest] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/test')
+      .then((response) => {
+        console.log(response.data);
+        setTest(response.data);
+      })
+      .catch((err) => {
+
+      })
+  }, []);
+
   return (
-    <div class="text-3xl font-bold underline">
-      Hello
-    </div>
+    <ul>
+      {Object.values(test).map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   );
 }
 
