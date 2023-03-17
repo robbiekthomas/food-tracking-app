00e5bb3e2eaa911d4fa29f1d2bb7c8958acf4f21
Lauren Johnston <hilotraining@outlook.com>
2023-03-17 12:43:53 -0600

    set up db server with robbie

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,18 +1,22 @@
 //PG database client / connection setup
 const { Pool } = require('pg');
+console.log('pool 1', Pool);
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: 'localhost',
+  port: 5432,
+  user: 'nutritionship',
+  // password: process.env.DB_PASS,
+  database: 'final'
 };
 
+
+
 const db = new Pool(dbParams);
+
 
 db.connect()
   .then(() => { console.log('connected to db') })
   .catch((err) => { console.log(err) });
-
+console.log(db);
 module.exports = db;
--- server/routes/dbController.js
+++ server/routes/dbController.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const db = require('../db/connection');
+const data = [1, 2, 3, 4, 5];
+// 1. you will receive the the id from react
+// 2. write query to get data from database
+// 3. send the data back to the react as a response
+//res.json(data);
 
-module.exports = (db) => {
-  router.get("/user", (req, res) => {
-    // 1. you will receive the the id from react
-    // 2. write query to get data from database
-    const userQueryStr = `
-    SELECT id, name, email, password
+
+router.get("/", (req, res) => {
+  console.log('getting data!');
+
+  const userQueryStr = `
+    SELECT *
     FROM users
     `;
-   const data = db.query(userQueryStr,)
+  db.query(userQueryStr)
     .then((result) => {
-      return result.rows[0]
+      const data = result.rows[0];
+      console.log('data is:', result.rows[0])
+      res.json(data);
     })
-    // 3. send the data back to the react as a response
-    res.json(data);
-  });
-};
+    .catch((err) => {
+      console.error(err);
+    })
+
+});
+
+
+module.exports = router;
