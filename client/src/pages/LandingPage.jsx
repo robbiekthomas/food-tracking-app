f53688b7beabf39f4c4e0a80b8863323d20560f6
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-03-18 00:16:40 -0600

    Initialize auth0 and Login Button

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import LoginButton from '../components/LoginButton'
 
 const LandingPage = () => {
   return (
     <div>
       <h1>Landing Page</h1>
+      
+      <LoginButton />
     </div>
+
   )
 }
 
