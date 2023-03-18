9282c85bffec061ee3bf9bfe1b685f717cd758e1
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-03-18 00:54:57 -0600

    Implement Logout Button and UserProfile

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,12 +1,25 @@
+import { User } from '@auth0/auth0-react'
 import React from 'react'
 import LoginButton from '../components/LoginButton'
+import LogoutButton from '../components/LogoutButton'
+import UserProfile from '../components/UserProfile'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const LandingPage = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
     <div>
       <h1>Landing Page</h1>
-      
+
       <LoginButton />
+      <LogoutButton />
+      
+  {/* User Profile should show on Dashboard */}
+      <UserProfile />
+
     </div>
 
   )
