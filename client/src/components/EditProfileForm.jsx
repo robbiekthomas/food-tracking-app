cc4f175f96b82e0578d7f903f14879a9475a608e
Lauren Johnston <hilotraining@outlook.com>
2023-03-18 11:01:03 -0600

    working on post request for db page

---
+++ client/src/components/EditProfileForm.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import { getUserRow } from '../api-requests/index';
+// import Button from '@mui/material/Button';
+// import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import { TextField, InputLabel, Button } from '@mui/material';
+
+
+const EditProfileForm = () => {
+  //temporary state container for user inputs
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [birthdate, setBirthdate] = useState("")
+  const [sex, setSex] = useState("")
+
+  const onNameChange = (e) => setName(e.target.value);
+  const onEmailChange = (e) => setEmail(e.target.value);
+  const onBirthdateChange = (e) => setBirthdate(e.target.value);
+  const onSexChange = (e) => setSex(e.target.value);
+
+  //will store the users old data technically then get submitted as package for post request
+  const [inputs, setUserInputs] = useState({
+    name: '',
+    email: '',
+    birthdate: '',
+    sex: ''
+  });
+
+  //gets user from the database
+  useEffect(() => {
+    getUserRow()
+      .then((res) => {
+        setUserInputs(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+
+  const validateSubmission = () => {
+    const submissionValues = ({
+      ...inputs,
+      name,
+      email,
+      birthdate,
+      sex
+    });
+
+    //setUserInputs(submissionValues);
+    updateDatabase(submissionValues);
+  }
+
+
+  const updateDatabase = (values) => {
+    const url = 'http://localhost:8000/api/dashboard/user/insert';
+    
+    console.log(values);
+    axios.post(url, values)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+
+  return (
+    <>
+      <h1>Edit Profile</h1>
+      <form>
+        <TextField
+          name='name'
+          value={name}
+          type='text'
+          onChange={(e) => onNameChange(e)}
+          placeholder={inputs.name}
+        />
+        <br />
+        <TextField
+          name='email'
+          type='email'
+          onChange={(e) => onEmailChange(e)}
+          placeholder={inputs.email}
+        />
+        <br />
+        <TextField
+          name='birthdate'
+          type='date'
+          onChange={(e) => onBirthdateChange(e)}
+        />
+        <br />
+        <TextField
+          name='sex'
+          type='text'
+          onChange={(e) => onSexChange(e)}
+          placeholder={inputs.sex}
+        />
+        <br />
+
+        <Button variant="contained" onClick={validateSubmission}>Input Data</Button>
+      </form>
+
+
+
+
+    </>
+  )
+}
+
+export default EditProfileForm;
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,55 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { getUserRow } from '../api-requests/index';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import axios from 'axios';
-import Box from '@mui/material/Box';
+import React from 'react';
+
+import EditProfileForm from '../components/EditProfileForm';
 
 const DashboardPage = () => {
-  const [test, setTest] = useState([]);
-
-  useEffect(() => {
-    getUserRow()
-      .then((res) => {
-        setTest(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  const [textValue, setTextValue] = useState("")
-  const onTextChange = (e) => setTextValue(e.target.value);
-  // const handleSubmit = () => console.log(textValue);
-  const handleSubmit = () => {
-
-    // const goal = {
-    //   name: textValue
-    // };
-
-    axios.post('http://localhost:8000/api/dashboard/habitGoals/insert', { textValue })
-      .then((res) => {
-        console.log(res);
-        console.log(res.data);
-      })
-      .catch((err) => {
-
-      })
-      
-  };
 
   return (
     <div>
-      <h1>Dashboard Page</h1>
-      <ul>
-        {Object.values(test).map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-
-      <TextField id="standard-basic" label="Standard" variant="standard" onChange={onTextChange} value={textValue} />
-      <Button variant="contained" onClick={handleSubmit}>Input Data</Button>
-
+      < EditProfileForm />
     </div>
   )
 }
