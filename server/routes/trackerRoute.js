d2a01d5da0ae8a5b64db32f3a16f82297954985a
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-03-20 21:17:16 -0600

    Implement grid component for food tracker and establish api route for food

--- server/routes/trackerRoute.js
+++ server/routes/trackerRoute.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../db/connection');
+const db = require("../db/connection");
+const axios = require("axios");
+require("dotenv").config();
+
+router.get("/", (req, res) => {
+  console.log("getting data!");
+
+  const userQueryStr = `
+  SELECT *
+  FROM foods
+      `;
+
+  db.query(userQueryStr)
+    .then((result) => {
+      const data = result.rows;
+      res.json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
 
 module.exports = router;
