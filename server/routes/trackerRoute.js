2af139fc8a5747b81a7e56ff7045ee09514b2ec8
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-03-21 17:20:09 -0600

    Make axios post request to food tracker db

--- server/routes/trackerRoute.js
+++ server/routes/trackerRoute.js
@@ -26,14 +26,22 @@ router.post("/food-log", (req, res) => {
   console.log("receiving data...")
 
   const logQueryStr = `
-    INSERT INTO food_logs (food_id, user_id, meal_id, servings)
+    INSERT INTO food_logs (food_id, user_id, meal_id)
     VALUES ($1, $2, $3)
     `;
-  db.query(logQueryStr, [
-    console.log(req.body)
-  ])
+  
+  console.log("req.body", req.body);
+  const values = [req.body.food_id, req.body.user_id, req.body.meal_id]
+
+  console.log("values", values);
+
+  db.query(logQueryStr, values)
   .then((res) => {
-    
+    console.log("logQueryResult", res.rows)
+    return res;
+  })
+  .catch((err) => {
+    console.log(err.message);
   })
 
 })
